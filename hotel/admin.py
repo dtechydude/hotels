6af9039509e7dfa87b4c7a2375c200b1baf0ca7a
Gbenga George <dtechydude@gmail.com>
2022-08-16 12:19:01 +0100

    additions

--- hotel/admin.py
+++ hotel/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 from .models import Room, Booking
 
 # Register your models here.
-admin.site.register(Room)
+class HotelAdmin(admin.ModelAdmin):
+    list_display = ('user', 'room', 'check_in', 'check_out',)
+    search_fields = ('user', 'room', 'check_in', 'check_out',)
 
-admin.site.register(Booking)
+
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('number', 'category', 'beds', 'capacity',)
+
+admin.site.register(Room, RoomAdmin)
+
+admin.site.register(Booking, HotelAdmin)
